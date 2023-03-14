4a3d82e46e4d6cc4ca0f179771c6d69b5a69b133
ksusha <ktsykanova@yandex.ru>
2023-03-14 18:06:42 +0300

    make first url

--- dogsite/homepage/views.py
+++ dogsite/homepage/views.py
@@ -1,1 +1,6 @@
-# from django.shortcuts import render
+from django.shortcuts import render
+
+
+def home(request):
+    template_name = 'homepage/index.html'
+    return render(request, template_name, {})
