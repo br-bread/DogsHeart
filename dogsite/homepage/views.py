8ca38ca016c88bea758d0932bb33ab856f77c732
ksusha <ktsykanova@yandex.ru>
2023-04-24 18:34:47 +0300

    update homepage views

--- dogsite/homepage/views.py
+++ dogsite/homepage/views.py
@@ -4,8 +4,3 @@ from django.shortcuts import render
 def home(request):
     template_name = 'homepage/index.html'
     return render(request, template_name, {})
-
-
-def recs(request):
-    template_name = 'homepage/index.html'
-    return render(request, template_name, {})
