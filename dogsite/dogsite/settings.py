82c4d1dceb615aa8cbcd2905ed7da0953a80c673
ksusha <ktsykanova@yandex.ru>
2023-06-08 18:59:56 +0300

    omg i cant use heroku i am dumb as frog

--- dogsite/dogsite/settings.py
+++ dogsite/dogsite/settings.py
@@ -23,7 +23,7 @@ SECRET_KEY = os.getenv('DJANGO_SECRET_KEY',
 
 DEBUG = bool(os.getenv('DEBUG', default=True))
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['a0818669.xsph.ru', '127.0.0.1']
 
 INSTALLED_APPS = [
     'django.contrib.admin',
@@ -43,7 +43,6 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
-    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -115,7 +114,6 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = [
     BASE_DIR / 'static'
 ]
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
